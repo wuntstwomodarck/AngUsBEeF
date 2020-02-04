37aed6f39cea23783dbe1da7358f603032aedd35
Jordan Bosch <jordanbosch75@gmail.com>
2020-02-03 17:04:19 -0700

    initialCommit

---
+++ BrehMomunt.js
@@ -0,0 +1,8 @@
+var AB=document.querySelector("#AB");
+            var Beaf=document.querySelector("#Beaf")
+            AB.onclick=function(){
+                console.log("Everybody Gangsta till Mom finds the piss couch");
+                
+                Beaf.innerHTML="<img src=\" https://cdn.shopify.com/s/files/1/2530/7762/products/wff-ribeye-steak-thick-choice-angus-lbs-vsp-raw-018-1_x750.progressive.jpg?v=1571713006\" >"
+            }
+        
